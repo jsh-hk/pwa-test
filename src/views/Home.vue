be7b860f72be9bd0075b90f9273d614f9efaff26
Joshua Hoke <joshua.hoke@gmail.com>
2018-10-31 17:15:49 -0400

    Remove HelloWorld.  Add PWA stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br>
+    <button>Enable notifications</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style scoped>
+button {
+  background: #41b883;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 18px;
+  margin-top: 20px;
+}
+</style>
