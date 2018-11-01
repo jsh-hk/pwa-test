cd20456ea600a9a7bc08f5f60f0a5a4d7e7a0733
Joshua Hoke <joshua.hoke@gmail.com>
2018-11-01 11:01:08 -0400

    Add notifications

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <br>
-    <button>Enable notifications</button>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    ><br>
+    <button
+      v-if="notificationsSupported"
+      @click="askPermission"
+    >Enable notifications</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
-  name: 'home'
+  name: 'Home',
+
+  data () {
+    return {
+      notificationsSupported: true
+    }
+  },
+  created () {
+    if ('Notification' in window && 'serviceWorker' in navigator) {
+      this.notificationsSupported = true
+    }
+  },
+
+  methods: {
+    askPermission () {
+      if (this.notificationsSupported) {
+        Notification.requestPermission(result => {
+          console.log('Result from permission question: ', result)
+          if (result !== 'granted') {
+            alert('y u no like notifications?!')
+          } else {
+            console.log('A notification will be send from the service worker => This only works in production')
+            this.showNotification()
+          }
+        })
+      }
+    },
+    showNotification () {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready // returns a Promise, the active SW registration
+          .then(swreg => swreg.showNotification('Notifications granted', {
+            body: 'Consider yourself notified',
+            icon: '/img/icons/android-chrome-192x192.png',
+            image: '/img/autumn-forest.png',
+            vibrate: [300, 200, 300],
+            badge: '/img/icons/plint-badge-96x96.png'
+            // actions: [
+            //     { action: 'confirm', title: 'Okay', icon: '/img/icons/android-chrome-192x192.png'},
+            //     { action: 'cancel', title: 'Cancel', icon: '/img/icons/android-chrome-192x192.png'}
+            // ],
+          }))
+      }
+    }
+  }
 }
 </script>
 
